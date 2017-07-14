79e0792e803e0d7a68fb84934c04089d42e12894
Andrey <fordcoppola@mail.ru>
2017-07-14 22:52:02 +0300

    готов

--- js/catalog.js
+++ js/catalog.js
@@ -1,12 +1,15 @@
 var link = document.querySelector(".map-content-btn");
 var popup = document.querySelector(".write-form");
 var popup_off = document.querySelector(".btn-contacts-exit");
+var overlay = document.querySelector(".overlay");
 
 link.addEventListener("click", function (event) {
     event.preventDefault();
     popup.classList.add("write-form-show");
+    overlay.style.display = "block";
 });
 popup_off.addEventListener("click", function (event) {
     event.preventDefault();
     popup.classList.remove("write-form-show");
+    overlay.style.display = "none";
 });
--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,8 @@
 var link = document.querySelector(".map-content-btn");
 var popup = document.querySelector(".write-form");
 var popup_off = document.querySelector(".btn-contacts-exit");
+var overlay = document.querySelector(".overlay");
+
 var radio_1 = document.querySelector(".slider-radio-first");
 var radio_2 = document.querySelector(".slider-radio-second");
 var radio_3 = document.querySelector(".slider-radio-third");
@@ -29,8 +31,10 @@ radio_3.addEventListener("click", function () {
 link.addEventListener("click", function (event) {
     event.preventDefault();
     popup.classList.add("write-form-show");
+    overlay.style.display = "block";
 });
 popup_off.addEventListener("click", function (event) {
     event.preventDefault();
     popup.classList.remove("write-form-show");
+    overlay.style.display = "none";
 });
